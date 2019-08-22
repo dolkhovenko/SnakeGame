7ac92ea910e6c4f8b64e1bc48596772a0d0456aa
dolkhovenko <39912170d>
2019-08-22 18:21:39 +0300

    Fix three heads

--- src/main/java/Snake.java
+++ src/main/java/Snake.java
@@ -5,8 +5,8 @@ class Snake {
 
     Snake() {
         snake.add(new BlockPosition(100, 20, BlockPosition.PartOfBody.HEAD));
-        snake.add(new BlockPosition(80, 20, BlockPosition.PartOfBody.HEAD));
-        snake.add(new BlockPosition(60, 20, BlockPosition.PartOfBody.HEAD));
+        snake.add(new BlockPosition(80, 20, BlockPosition.PartOfBody.BODY));
+        snake.add(new BlockPosition(60, 20, BlockPosition.PartOfBody.BODY));
         snake.add(new BlockPosition(40, 20, BlockPosition.PartOfBody.BODY));
         snake.add(new BlockPosition(20, 20, BlockPosition.PartOfBody.TAIL));
     }
